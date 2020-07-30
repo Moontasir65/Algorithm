9efac95f31803c92ad1c421717fba7d864cae1ea
Moontasir moon <66530168+Moontasir65@users.noreply.github.com>
2020-07-31 01:51:04 +0600

    Add files via upload

---
+++ coin changing/coin challange dinamic 1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void coin_change(int coin[],int totalCoin,int change)
+{
+    int m[change+1],minimum,i,j;
+    m[0]=0;
+    for(i=1; i<=change; i++)
+
+      {
+        minimum=change+1;
+        for(j=0; j<totalCoin; j++){
+            if(coin[j]<=i){
+                    if(m[i-coin[j]]+1 < minimum)
+                    minimum= m[i-coin[j]]+1;
+            }
+        }
+        m[i]=minimum;
+    }
+    if(m[change]==0)
+        cout<<"Change is not possible"<<endl;
+    else
+      cout<<"Coin need: "<<endl<<m[change]<<endl;
+}
+    int main() {
+    int i,totalCoin=4,change=16;
+    int coin[]= {1,2,8,12};
+    coin_change(coin,totalCoin,change);
+    return 0;
+}
